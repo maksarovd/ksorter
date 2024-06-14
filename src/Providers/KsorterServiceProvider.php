51e58e45a02dce5ba157161decadea0e884a4b7c
maksarovd <bondspear2020@gmail.com>
2024-06-14 09:31:52 +0300

    v2

---
+++ src/Providers/KsorterServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Maksarovd\Ksorter\Providers;
+
+use Illuminate\Support\{ServiceProvider};
+
+
+class KsorterServiceProvider extends ServiceProvider
+{
+
+
+    public function register(): void
+    {
+        //
+    }
+
+
+
+    public function boot(): void
+    {
+        //
+    }
+}
