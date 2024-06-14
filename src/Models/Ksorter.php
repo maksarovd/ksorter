4807099b17b697a8227e49f8abf05028e68f98d7
maksarovd <bondspear2020@gmail.com>
2024-06-14 09:09:59 +0300

    initial commit

---
+++ src/Models/Ksorter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Maksarovd\KSorter\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Ksorter extends Model
+{
+    use HasFactory;
+
+}
